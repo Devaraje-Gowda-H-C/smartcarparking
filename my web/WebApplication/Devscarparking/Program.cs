2a19654415209020c7db2716f2d10f33d45454f2
Devaraje-Gowda-H-C <devarajgowdahc@gmail.com>
2025-04-10 13:31:58 +0530

    Update Program.cs

--- my web/WebApplication/Devscarparking/Program.cs
+++ my web/WebApplication/Devscarparking/Program.cs
@@ -2,19 +2,19 @@ using Devscarparking.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllers();
 
-// Register your ParkingManager service as a Singleton (1 shared instance)
+
 builder.Services.AddSingleton<ParkingManager>();
 
-// Add Swagger/OpenAPI support
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
